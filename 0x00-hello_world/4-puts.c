24f69c961f775dac639ce767446ac17dda459a0d
Mabellian <mabellian4@gmail.com>
2022-09-08 17:05:48 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+/**
+ * Description: main - Print out a string to stdout
+ * Return: Return 0 for success
+ */
 
-/*
- * *
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(void)
 {
+ 
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        return(0);
 }
