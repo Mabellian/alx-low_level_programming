a1a413ac3a172c0ef3fc89eb70a5fb6667db30b5
Mabellian <mabellian4@gmail.com>
2022-09-11 10:30:20 -0700

    combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio>
+
+/**
+ * main - print all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int c, i;
+
+	c = i = '0';
+
+	for (c = '0'; c <= '9'; c++)
+
+		for (i = '0'; i <= '9'; i++)
+
+			putchar(c);
+
+	                putchar(i);
+
+
+			if ((c != '9') || (c == '9' && i != '9'))
+
+				putchar(',');
+			putchar(' ');
+
+			putchar('\n');
+
+			return (0);
+
+}
