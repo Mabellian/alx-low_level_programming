f6f7cd302cb465a7ad13fe014439ff8a0366bb36
Mabellian <mabellian4@gmail.com>
2022-09-14 13:48:18 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - alphabets in lowercase
+ * print_alphabet - Alphabets in lowercase
  *
  * Return: Always 0
  */
@@ -14,5 +14,5 @@ void print_alphabet(void)
 
 		_putchar(c);
 
-	_putchar('\n')
+	_putchar('\n');
 }
