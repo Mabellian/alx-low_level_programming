69bd176824e8875ca81fbfff5c9603d7d396fcf0
Mabellian <mabellian4@gmail.com>
2022-09-28 20:41:04 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - return the square root 
+ * _sqrt_recursion - return the natural square root of a number
  * @n: number to calculate the square root of
  * Return: the resulting square root
  */
